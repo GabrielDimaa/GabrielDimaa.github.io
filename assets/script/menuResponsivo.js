c8b21f2c783a2a33548718e872446b7a6088533f
Gabriel de Matos Hainzenreder <gabrielmhainzenreder0@gmail.com>
2026-01-17 22:11:08 -0300

    Refeito o portfólio do zero 🌟

--- assets/script/menuResponsivo.js
+++
@@ -1,8 +0,0 @@
-function responsive() {
-  	let menu = document.getElementById("menu-id")
-  	if (menu.className === "menu") {
-    	menu.className += " responsive"
-  	} else {
-    	menu.className = "menu"
-  	}
-}
